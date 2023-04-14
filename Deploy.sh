606c0a1e7cb44145d3e400e67702f7e937a40d0a
William Rezende <wrassis84@gmail.com>
2023-04-13 21:36:21 -0300

    feat: add script to deploy nfs-sharing

---
+++ Deploy.sh
@@ -0,0 +1,32 @@
+######## ENVIRONMENT ###########################################################
+
+#!/usr/bin/env bash
+
+######## ABOUT #################################################################
+
+# Repository      : https://github.com/wrassis84/<REPO>
+# Author          : William Ramos de Assis Rezende
+# Maintainer      : William Ramos de Assis Rezende
+#
+# Deploy.sh       : Executes Ansible Playbooks to Deploy Servers.
+# Requirements    : bash, ansible
+# Usage           : DataTex.sh [ list | add | remove ]
+
+######## TESTING ENVIRONMENT ###################################################
+
+######## TESTS/VALIDATIONS #####################################################
+
+######## VARIABLES #############################################################
+
+######## FUNCTIONS #############################################################
+
+######## MAIN CODE - START #####################################################
+echo "Configuring environment..."
+source .env && cd - > /dev/null
+
+echo "Playing nfs-sharing role..."
+ansible-playbook main.yaml -u sysadmin -b #> /dev/null
+######## MAIN CODE - END #######################################################
+# TODO:
+# Test if ansible is installed;
+# Checks that servers are running and reachable;
